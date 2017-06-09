926295b8862cf3a57db31d3b912f1c9fe3eb773d
George Petrou <gspetrou98@gmail.com>
2017-06-09 00:07:58 -0400

    Front-end UI Overhaul

--- ozarrp/gamemode/shared.lua
+++ ozarrp/gamemode/shared.lua
@@ -5,8 +5,8 @@ GM.Website = "https://bitbucket.org/gonzalolog/2017-gamemode-coding-competition"
 
 local _, folder = file.Find(GM.FolderName .. "/gamemode/modules/*", "LUA")
 
-for _, dir in pairs(folder) do
-    for cs, fl in pairs(file.Find(GM.FolderName .. "/gamemode/modules/" .. dir .. "/cl_*.lua", "LUA")) do
+for _, dir in ipairs(folder) do
+    for cs, fl in ipairs(file.Find(GM.FolderName .. "/gamemode/modules/" .. dir .. "/cl_*.lua", "LUA")) do
         if SERVER then
             AddCSLuaFile(GM.FolderName .. "/gamemode/modules/" .. dir .. "/" .. fl)
         else
@@ -14,13 +14,13 @@ for _, dir in pairs(folder) do
         end
     end
 
-    for sh, fl in pairs(file.Find(GM.FolderName .. "/gamemode/modules/" .. dir .. "/sh_*.lua", "LUA")) do
+    for sh, fl in ipairs(file.Find(GM.FolderName .. "/gamemode/modules/" .. dir .. "/sh_*.lua", "LUA")) do
         AddCSLuaFile(GM.FolderName .. "/gamemode/modules/" .. dir .. "/" .. fl)
         include(GM.FolderName .. "/gamemode/modules/" .. dir .. "/" .. fl)
     end
 
     if SERVER then
-        for sv, fl in pairs(file.Find(GM.FolderName .. "/gamemode/modules/" .. dir .. "/sv_*.lua", "LUA")) do
+        for sv, fl in ipairs(file.Find(GM.FolderName .. "/gamemode/modules/" .. dir .. "/sv_*.lua", "LUA")) do
             include(GM.FolderName .. "/gamemode/modules/" .. dir .. "/" .. fl)
         end
     end
